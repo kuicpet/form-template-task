310767fd52293eb56bc4e3b950d0263e3a6ac4cf
Kingsley <kingsleyumujeyan@gmail.com>
2021-05-23 20:40:32 +0100

    ft--components and pages added

--- src/Context.js
+++ src/Context.js
@@ -10,11 +10,32 @@ class TemplateProvider extends Component {
         sortedTemplates: [],
         loading: true,
         category: "all",
+        description: ""
     };
 
     //Get Templates 
     getTemplates = async () => {
-
+        const url = "https://front-end-task-dot-fpls-dev.uc.r.appspot.com/api/v1/public/task_templates"
+        try {
+            await fetch(url)
+            .then(response => {
+                if(response.ok){
+                    return response.json()
+                } else {
+                    return Promise.reject(response);
+                }
+            })
+            .then(data => {
+                console.log(data);
+                this.setState({
+                    templates,
+                    sortedTemplates,
+                    loading: false,
+                })
+            })
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     componentDidMount(){
---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactLoading from "react-loading";
+
+export default function Loading() {
+    return (
+        <div className="loading">
+            <div className="load">
+                <ReactLoading type="spin" color="#023e8a" width={30} height={30} />
+            </div>
+            <h4>loading templates...</h4>
+        </div>
+    )
+}
