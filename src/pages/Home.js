d948cee4099d470988d82247a51b90bfbc326e78
Kingsley <kingsleyumujeyan@gmail.com>
2021-05-24 01:51:49 +0100

    ft--filter section ui added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from 'react';
+
 
 export default function Home() {
     return (
-        <div>
-            Home Page
+        <div className="container">
+            <div className="filter_container">
+                <div className="search_container">
+                    <form>
+                        <input 
+                            className="search_bar"
+                            type="text"
+                            placeholder="Search Templates"
+                        />
+                    </form>
+                </div>
+                <div className="sort_container">
+                    <span>
+                        Sort by:
+                    </span>
+                    <form>
+                        <fieldset>
+                            <legend>Category</legend>
+                            <div className="select">
+                                <select>
+                                    <option>All</option>
+                                    <option>Education</option>
+                                    <option>E-Commerce</option>
+                                    <option>Health</option>
+                                </select>
+                            </div>
+                        </fieldset>
+                        <fieldset>
+                            <legend>Order</legend>
+                            <div className="select">
+                                <select>
+                                    <option>Default</option>
+                                    <option>Ascending</option>
+                                    <option>Descending</option>
+                                </select>
+                            </div>
+                        </fieldset>
+                        <fieldset>
+                            <legend>Date</legend>
+                            <div className="select">
+                                <select>
+                                    <option>Default</option>
+                                    <option>Ascending</option>
+                                    <option>Descending</option>
+                                </select>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
